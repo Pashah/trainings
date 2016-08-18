4a1a90af3934fb683d8215f4a724478dc7f283c8
Miika Kalske <miika.kal@gmail.com>
2016-08-18 18:43:06 +0300

    Refactored users route, made it more DRY, use of promises

--- routes/users.js
+++ routes/users.js
@@ -1,51 +1,37 @@
 var express = require('express');
 var router = express.Router();
-var pg = require('pg');
 var path = require('path');
+var Promise = require('bluebird');
+var pgp = require('pg-promise')();
 var connectionString = require(path.join(__dirname, '../', 'config.js'));
+var db = pgp(connectionString);
+
+var handleError = function(err) {
+  console.log(err);
+  return res.status(500).json({data: err});
+}
 
 router.get('', function(req, res, next) {
-
-  var results = [];
-  pg.connect(connectionString, function(err, client, done) {
-        // Handle connection errors
-        if(err) {
-          done();
-          console.log(err);
-          return res.status(500).json({ success: false, data: err});
-        }
-        // SQL Query > Insert Data
-        var query = client.query("SELECT id, name FROM users");
-        query.on('row', function(row) {
-            results.push(row);
-        });
-        query.on('end', function() {
-             done();
-             return res.json(results);
-        });
+  db.query("SELECT id, name FROM users")
+    .then(function (data) {
+      return res.json(data);
     });
 });
 
 router.get('/:userId', function(req, res, next) {
-  res.send('get one user with id ' + req.params.userId);
+  var id = req.params.userId;
+  db.query("SELECT id, name FROM users WHERE id = $1", [id])
+    .then(function (data) {
+      return res.json(data);
+    });
 });
 
 router.post('', function(req, res, next) {
-
   var data = {name: req.body.name};
-
-  pg.connect(connectionString, function(err, client, done) {
-        // Handle connection errors
-        if(err) {
-          done();
-          console.log(err);
-          return res.status(500).json({ success: false, data: err});
-        }
-        // SQL Query > Insert Data
-       client.query("INSERT INTO users(name) values($1)", [data.name]);
-
-       res.send('User created with name: ' + data.name);
-    });
+  db.none(
+    "INSERT INTO users(name) values($1)", [data.name]
+  );
+  res.send('User created with name: ' + data.name);
 });
 
 module.exports = router;
