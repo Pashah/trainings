bb35165191694728a3c7a0570789489b23292f05
Miika Kalske <miika.kal@gmail.com>
2016-08-22 15:54:05 +0300

    Added category to trainings

--- front/app/scripts/controllers/training.js
+++ front/app/scripts/controllers/training.js
@@ -14,6 +14,9 @@ angular.module('cfFrontApp')
     });
   })
   .controller('TrainingCtrl', function ($scope, $http) {
+    var categories = $http.get("http://localhost:8080/categories").success(function(data, status) {
+      $scope.categories = data;
+    });
     $scope.submit = function() {
       var training = $scope.training;
       console.log("Created by user id: " + training.created_by);
